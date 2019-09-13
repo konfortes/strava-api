eff60db2f3591ccc4dc03449802040625d47a5b5
konfortes <ronen.konfortes@riskified.com>
2019-09-14 00:49:08 +0300

    fix webhook

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,19 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:strava]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    existing_user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email || "no_email#{Time.current.to_i}@stam.com"
       user.password = Devise.friendly_token[0, 20]
       user.authorization_token = auth.credentials.token
     end
+
+    if existing_user.authorization_token != auth.credentials.token
+      existing_user.authorization_token = auth.credentials.token
+      existing_user.save!
+    end
+
+    existing_user
   end
 end

e56c951f0b4a02376ccc448028780431c5e967a9
konfortes <ronen.konfortes@riskified.com>
2019-09-15 22:36:30 +0300

    redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,8 @@
 # REDIS_CONFIG = YAML.load(File.open( Rails.root.join("config/redis.yml") ) ).deep_symbolize_keys
 
 $redis = Redis.new(
-  host: ENV.fetch('REDIS_HOST', 'localhost'),
-  port: ENV.fetch('REDIS_PORT', 6379),
-  db: ENV.fetch('REDIS_DB', 0)
+  url: ENV.fetch('REDIS_URL', 'localhost:6379/0')
+  # host: ENV.fetch('REDIS_HOST', 'localhost'),
+  # port: ENV.fetch('REDIS_PORT', 6379),
+  # db: ENV.fetch('REDIS_DB', 0)
 )

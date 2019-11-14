af52d42e09fbb10923ee493b0a54c3faf252e56c
konfortes <ronen.konfortes@riskified.com>
2019-11-14 17:53:19 +0200

    remove postgis extension and entities

--- db/migrate/20190921185641_add_postgis.rb
+++
@@ -1,6 +0,0 @@
-class AddPostgis < ActiveRecord::Migration[5.0]
-  def change
-    enable_extension :postgis
-    # ActiveRecord::Base.connection.execute('CREATE EXTENSION IF NOT EXISTS postgis')
-  end
-end
